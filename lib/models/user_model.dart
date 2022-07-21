f891ad95e707327d1f3381dc12e5cf33a24790ee
MayuRuru <aruizrull@protonmail.com>
2022-07-21 10:59:08 +0200

    Add fromJson to UserModel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,4 +1,4 @@
-class UserModel{
+class UserModel {
   final int id;
   final String name;
   final String email;
@@ -21,6 +21,56 @@ class UserModel{
     this.avatar = "",
     this.rating = 0,
     this.emps = 0,
-});
+  });
 
+  UserModel copy({
+    String? name,
+    int? id,
+    String? email,
+    String? password,
+    String? invitation,
+    String? location,
+    bool? isActive,
+    String? avatar,
+    int? rating,
+    int? emps,
+  }) =>
+      UserModel(
+        name: name ?? this.name,
+        id: id ?? this.id,
+        email: email ?? this.email,
+        password: password ?? this.password,
+        invitation: invitation ?? this.invitation,
+        location: location ?? this.location,
+        isActive: isActive ?? this.isActive,
+        avatar: avatar ?? this.avatar,
+        rating: rating ?? this.rating,
+        emps: emps ?? this.emps,
+      );
+
+  static UserModel fromJson(Map<String, dynamic> json) => UserModel(
+        name: json['name'],
+        id: json['id'],
+        email: json['email'],
+        password: json['password'],
+        invitation: json['invitation'],
+        location: json['location'],
+        avatar: json['avatar'],
+        rating: json['rating'],
+        isActive: json['isActive'],
+        emps: json['emps'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'id': id,
+        'email': email,
+        'password': password,
+        'invitation': invitation,
+        'location': location,
+        'avatar': avatar,
+        'rating': rating,
+        'isActive': isActive,
+        'emps': emps,
+      };
 }

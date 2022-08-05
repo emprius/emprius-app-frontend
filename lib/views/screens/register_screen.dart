b70e42238741a0ed265400cd0909ace60a3bf0d8
MayuRuru <aruizrull@protonmail.com>
2022-08-05 09:43:57 +0200

    Add persistance for avatar and isactive fields

--- lib/views/screens/register_screen.dart
+++ lib/views/screens/register_screen.dart
@@ -1,16 +1,13 @@
-//import 'package:empriusapp/models/user_model.dart';
+import 'dart:io';
 import 'package:empriusapp/models/user_model.dart';
-import 'package:empriusapp/providers/map_providers.dart';
 import 'package:empriusapp/providers/user_provider.dart';
 import 'package:empriusapp/routes/routes.dart';
 import 'package:empriusapp/utils/constants.dart';
 import 'package:empriusapp/views/widgets/common/custom_text_button.dart';
-//import 'package:empriusapp/views/widgets/test_widget.dart';
 import 'package:empriusapp/views/widgets/user_map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:empriusapp/utils/form_validator.dart';
-
 import '../widgets/common/bottom_image_selector.dart';
 import '../widgets/common/custom_textfield.dart';
 import '../widgets/profile_image_widget.dart';
@@ -23,17 +20,17 @@ class RegisterScreen extends ConsumerStatefulWidget {
 }
 
 class _RegisterScreenState extends ConsumerState<RegisterScreen> {
-
   final _formKey = GlobalKey<FormState>();
   final _nameCtrl = TextEditingController();
   final _emailCtrl = TextEditingController();
   final _passwordCtrl = TextEditingController();
   final _cPasswordCtrl = TextEditingController();
   final _invitationCtrl = TextEditingController();
-  final _mapController =  UserMapController(
-      validator: FormValidator.locationNullValidator);
+  final _mapController =
+      UserMapController(validator: FormValidator.locationNullValidator);
 
   late bool isActive = true;
+  File? _avatar;
 
   @override
   void dispose() {
@@ -73,21 +70,20 @@ class _RegisterScreenState extends ConsumerState<RegisterScreen> {
                           showModalBottomSheet(
                             context: context,
                             builder: ((builder) => BottomImageSelector((image) {
-                              //TODO IMPLEMENT CONTROLLER?
-                              // if (image != null) {
-                              //   user = user.copy(avatar: image.path);
-                              //   setState(() {});
-                              // }
-                              Navigator.pop(context);
-                            }
-                            )
-                            ),
+                                  _avatar = image;
+                                  setState((){});
+                                  Navigator.pop(context);
+                                })),
                           );
                         },
                         child: SizedBox(
                           height: 100,
                           width: 100,
-                          child: ProfileImage(showBadge: true, badgeIcon: Icons.camera_alt, avatar: defaultAvatar,),
+                          child: ProfileImage(
+                            showBadge: true,
+                            badgeIcon: Icons.camera_alt,
+                            avatar: _avatar == null ? defaultAvatar : _avatar!.path,
+                          ),
                         ),
                       ),
                     ],
@@ -161,7 +157,8 @@ class _RegisterScreenState extends ConsumerState<RegisterScreen> {
                   CustomTextButton(
                     text: 'Finalitza registre',
                     onClicked: () {
-                      if (!_formKey.currentState!.validate() && !_mapController.validate()) {
+                      if (!_formKey.currentState!.validate() &&
+                          !_mapController.validate()) {
                         return;
                       }
                       ScaffoldMessenger.of(context).showSnackBar(
@@ -175,6 +172,8 @@ class _RegisterScreenState extends ConsumerState<RegisterScreen> {
                             password: _passwordCtrl.text,
                             invitation: _invitationCtrl.text,
                             location: _mapController.selectedLocation!,
+                            avatar: _avatar?.path ?? "",
+                            isActive: isActive,
                           ));
 
                       Navigator.pushReplacementNamed(
@@ -190,22 +189,22 @@ class _RegisterScreenState extends ConsumerState<RegisterScreen> {
     );
   }
 
-
-  Widget selectLocationMap( ) => Column(
-    children: [
-      const Text('Localitzacio actual:'),
-      const SizedBox(height: 6.0),
-      Container(
-        width: 300,
-        height: 150,
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black26),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: UserMap(controller: _mapController,),
-      ),
-    ],
-  );
-
+  Widget selectLocationMap() => Column(
+        children: [
+          const Text('Localitzacio actual:'),
+          const SizedBox(height: 6.0),
+          Container(
+            width: 300,
+            height: 150,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black26),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: UserMap(
+              controller: _mapController,
+            ),
+          ),
+        ],
+      );
 }

d80c7290601b91f8394d872c9b98c382f12f824c
MayuRuru <aruizrull@protonmail.com>
2022-09-16 18:29:44 +0200

    Finish bookings providers

--- lib/src/features/bookings/application/providers/bookings_providers.dart
+++ lib/src/features/bookings/application/providers/bookings_providers.dart
@@ -1,8 +1,10 @@
 import 'package:empriusapp/src/features/bookings/application/notifiers/all_bookings_notifier.dart';
 import 'package:empriusapp/src/features/bookings/data/repositories/bookings_repository.dart';
 import 'package:empriusapp/src/features/bookings/domain/booking_model.dart';
+import 'package:empriusapp/src/features/bookings/domain/enums/booking_status_enum.dart';
 import 'package:empriusapp/src/features/user/auth_user/data/user_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 
 final allBookingsProvider = StateNotifierProvider<AllBookingsNotifier, List<BookingModel>>((ref) {
@@ -11,22 +13,36 @@ final allBookingsProvider = StateNotifierProvider<AllBookingsNotifier, List<Book
 });
 
 
-//TODO check this / how to add petitions:
-final authUserBookingsProvider = StateProvider<List<BookingModel>>((ref) {
+///To filter the list of bookings according to the booking status,
+///Create a StateProvider which exposes the currently selected filter method:
+final bookingFilterProvider = StateProvider<BookingStatus>((ref) => BookingStatus.ASKED);
+
+///Together with the global StateNotifierProvider that exposes a bookings list,
+///And a separate provider which combines the filter method and the bookings list:
+final filteredBookingProvider = Provider<List<BookingModel>>((ref){
+  final filterStatus = ref.watch(bookingFilterProvider);
+  final bookings = ref.watch(allBookingsProvider);
+
+  switch(filterStatus){
+    case BookingStatus.APPROVED:
+      return bookings.where((booking) => booking.bookingStatus == BookingStatus.APPROVED).toList();
+    case BookingStatus.ASKED:
+      return bookings.where((booking) =>  booking.bookingStatus == BookingStatus.ASKED).toList();
+    case BookingStatus.RETURNED:
+      return bookings.where((booking) => booking.bookingStatus == BookingStatus.RETURNED).toList();
+  }
+});
+
+///Requested bookings TO a user (outcoming):
+final requestBookingsProvider = StateProvider<List<BookingModel>>((ref) {
     var userId = ref.watch(userProvider.select((user) => user.id));
-return ref.watch(requestedBookingsByUserProvider(userId));
+    var allBookings = ref.watch(filteredBookingProvider);
+    return allBookings.where((booking) => booking.toUserId == userId).toList();
 });
 
-
-
-///Petitioned bookings FROM a user (incoming)
-final petitionedBookingsByUserProvider = StateProvider.family<List<BookingModel>, int>((ref, userId){
-  var petitionsList = ref.watch(allBookingsProvider).where((booking) => booking.fromUserId == userId).toList();
-  return petitionsList;
-});
-
-///Requested bookings TO a user (outcoming)
-final requestedBookingsByUserProvider = StateProvider.family<List<BookingModel>, int>((ref, userId){
-  var bookingsList = ref.watch(allBookingsProvider).where((booking) => booking.toUserId == userId).toList();
-  return bookingsList;
+///Petitioned bookings FROM a user (incoming):
+final petitionBookingsProvider = StateProvider<List<BookingModel>>((ref) {
+  var userId = ref.watch(userProvider.select((user) => user.id));
+  var allBookings = ref.watch(filteredBookingProvider);
+  return allBookings.where((booking) => booking.fromUserId == userId).toList();
 });

7a0826b278c68c856fd1b767142f19fe161c22fc
MayuRuru <aruizrull@protonmail.com>
2022-09-19 13:00:13 +0200

    Rebuild user info model

--- lib/src/features/bookings/domain/user_info/user_info_model.dart
+++ lib/src/features/bookings/domain/user_info/user_info_model.dart
@@ -1,14 +1,14 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'user_info_model.freezed.dart';
-//part 'user_info.g.dart';
+part 'user_info_model.g.dart';
 
 @freezed
 class UserInfo with _$UserInfo{
 
   const factory UserInfo({
-  required String? name,
-  int? rating,
-      String? avatar,
+    required String? name,
+    int? rating,
+    String? avatar,
   }) = _UserInfo;
 
 
---
+++ lib/src/features/bookings/domain/user_info/user_info_model.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_info_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserInfo _$$_UserInfoFromJson(Map<String, dynamic> json) => _$_UserInfo(
+      name: json['name'] as String?,
+      rating: json['rating'] as int?,
+      avatar: json['avatar'] as String?,
+    );
+
+Map<String, dynamic> _$$_UserInfoToJson(_$_UserInfo instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'rating': instance.rating,
+      'avatar': instance.avatar,
+    };

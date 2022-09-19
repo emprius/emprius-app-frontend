63d500bdb888276d3542f3be19a28d5e9631549b
MayuRuru <aruizrull@protonmail.com>
2022-09-19 16:51:24 +0200

    Add controllers and booking instance create

--- lib/src/features/bookings/presentation/screens/ask_tool_form_screen.dart
+++ lib/src/features/bookings/presentation/screens/ask_tool_form_screen.dart
@@ -1,6 +1,9 @@
 import 'package:empriusapp/src/core/common_widgets/custom_textfield.dart';
-import 'package:empriusapp/src/core/common_widgets/date_picker_widget.dart';
+import 'package:empriusapp/src/features/bookings/presentation/screens/fragments/widgets/date_picker_widget.dart';
 import 'package:empriusapp/src/core/routes.dart';
+import 'package:empriusapp/src/features/bookings/application/providers/bookings_providers.dart';
+import 'package:empriusapp/src/features/bookings/domain/booking_model.dart';
+import 'package:empriusapp/src/features/bookings/domain/enums/booking_status_enum.dart';
 import 'package:empriusapp/src/features/tool/application/providers/tool_providers.dart';
 import 'package:empriusapp/src/features/tool/domain/tool_model.dart';
 import 'package:empriusapp/src/features/user/emprius_user/presentation/widgets/user_appbar.dart';
@@ -17,7 +20,10 @@ class AskToolFormScreen extends ConsumerStatefulWidget {
 
 class _AskToolFormScreenState extends ConsumerState<AskToolFormScreen> {
   DateTime date = DateTime(2022, 8, 1);
-  late int id;
+
+  final _formKey =GlobalKey<FormState>();
+  final _contactCtrl = TextEditingController();
+  final _commentsCtrl = TextEditingController();
 
   @override
   void initState() {
@@ -31,8 +37,22 @@ class _AskToolFormScreenState extends ConsumerState<AskToolFormScreen> {
     return Scaffold(
       appBar: UserAppbar('Formulari contacte'),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-         //TODO implement create booking instance
+        onPressed: () async {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Peticio enviada!')),
+          );
+          await ref
+              .read(allBookingsProvider.notifier)
+              .createBooking(BookingModel(
+              bookingStatus: BookingStatus.ASKED,
+              contact: _contactCtrl.text,
+            comments: _commentsCtrl.text,
+            //userInfo:
+          ));
+
+          if (!mounted) return;
+          Navigator.pushNamed(
+              context, userActivityScreenRoute);
         },
         label: Text("Envia"),
       ),
@@ -40,7 +60,7 @@ class _AskToolFormScreenState extends ConsumerState<AskToolFormScreen> {
         padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
         physics: const BouncingScrollPhysics(),
         child: Form(
-          //key: _formKey,
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -48,18 +68,18 @@ class _AskToolFormScreenState extends ConsumerState<AskToolFormScreen> {
               Text('Demana la eina : ${tool.title}'),
               SizedBox(height: 20.0),
               CustomTextField(
-                //controller: _petitionCtrl,
+                controller: _contactCtrl,
                 labelText: "Especifica forma i dades de contacte",
               ),
               SizedBox(height: 20.0),
               CustomTextField(
-                //controller: _petitionCtrl,
+                controller: _commentsCtrl,
                 maxLines: 5,
                 labelText: "Pots afegir aqui comentaris adicionals",
               ),
               SizedBox(height: 20.0),
               DatePickerWidget("Selecciona dia d'inici"),
-              //TODO (m): pass data from widget
+              //TODO (m): pass data from widget > endDate and startDate
               Text('${date.year}/${date.month}/${date.day}'),
               DatePickerWidget("Selecciona data de tornada"),
               Text('${date.year}/${date.month}/${date.day}'),

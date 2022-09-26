29255010a04aba1bc4d95192465430853574d0dc
MayuRuru <aruizrull@protonmail.com>
2022-09-26 14:43:49 +0200

    Add thumbnails to ratings

--- lib/src/features/rating/data/mocked/mocked_ratings_service.dart
+++ lib/src/features/rating/data/mocked/mocked_ratings_service.dart
@@ -8,17 +8,24 @@ DateTimeRange(start: DateTime.now().add(const Duration(days: 1)), end: DateTime.
 DateTimeRange(start: DateTime.now().add(const Duration(days: 10)), end: DateTime.now().add(const Duration(days: 23))),
 ];
 
+var sampleThumbnails = [
+  '/data/user/0/com.example.empriusapp/cache/image_picker4192891788779617458.jpg',
+  '/data/user/0/com.example.empriusapp/cache/image_picker2314740962926778978.png',
+  '/data/user/0/com.example.empriusapp/cache/image_picker2519909079312788563.jpg',
+  '/data/user/0/com.example.empriusapp/cache/image_picker5181464323563610078.jpg'
+];
+
 var sampleRatings = [
   RatingModel(
     ratingId: 1,
     isPending: true,
     fromUserId: 1,
     toUserId: 2,
-    //title
-    //thumbnail
+    title: "User1",
+    thumbnail: sampleThumbnails[0],
     rating: 0,
-    bookingId: 1,
-    ratingType: RatingType.ALL,
+    bookingId: 1, //bici de passeig
+    ratingType: RatingType.USER_RATE,
     bookingReservedDates: sampleReservations,
   ),
   RatingModel(
@@ -26,23 +33,35 @@ var sampleRatings = [
     isPending: true,
     fromUserId: 1,
     toUserId: 2,
-    //title
-    //thumbnail
+    title: "User1.1",
+    thumbnail: sampleThumbnails[1],
     rating: 0,
-    bookingId: 1,
-    ratingType: RatingType.ALL,
+    bookingId: 2, //burra autonoma
+    ratingType: RatingType.USER_RATE,
     bookingReservedDates: sampleReservations,
   ),
   RatingModel(
     ratingId: 3,
     isPending: true,
-    fromUserId: 1,
-    toUserId: 2,
-    //title
-    //thumbnail
+    fromUserId: 2,
+    toUserId: 1,
+    title: "Tractor",
+    thumbnail: sampleThumbnails[2],
     rating: 0,
-    bookingId: 1,
-    ratingType: RatingType.ALL,
+    bookingId: 5, //Tractor,
+    ratingType: RatingType.TOOL_RATE,
     bookingReservedDates: sampleReservations,
-  )
+  ),
+  RatingModel(
+    ratingId: 4,
+    isPending: true,
+    fromUserId: 2,
+    toUserId: 1,
+    title: "Reg automatic",
+    thumbnail: sampleThumbnails[3],
+    rating: 0,
+    bookingId: 9, //Reg automatic,
+    ratingType: RatingType.TOOL_RATE,
+    bookingReservedDates: sampleReservations,
+  ),
 ];

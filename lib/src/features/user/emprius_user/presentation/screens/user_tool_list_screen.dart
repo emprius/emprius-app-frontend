0e21486043a14f492ed651b73eef3884eb623c22
MayuRuru <aruizrull@protonmail.com>
2022-09-27 19:25:02 +0200

    Correct listview overflow

--- lib/src/features/user/emprius_user/presentation/screens/user_tool_list_screen.dart
+++ lib/src/features/user/emprius_user/presentation/screens/user_tool_list_screen.dart
@@ -21,14 +21,23 @@ class _UserToolListState extends ConsumerState<UserToolList> {
 
 
   void _deleteTool(ToolModel tool) {
-   ref.watch(allToolsProvider.notifier).deleteTool(tool);
+    ref.watch(allToolsProvider.notifier).deleteTool(tool);
   }
 
   @override
   void initState() {
-    // Fetch tools by userId
-    ref.read(allToolsProvider.notifier).getAllByUser(userId: ref.read(userProvider).id);
+    /// Fetch tools by userId
+    ref.read(allToolsProvider.notifier).getAllByUser(userId: ref
+        .read(userProvider)
+        .id);
     super.initState();
+  }
+
+  Future<void> _refresh() async {
+    ref.watch(allToolsProvider.notifier)
+        .getAllByUser(userId: ref
+        .read(userProvider)
+        .id);
   }
 
   @override
@@ -39,82 +48,94 @@ class _UserToolListState extends ConsumerState<UserToolList> {
       appBar: UserAppbar("Les meves eines"),
       floatingActionButton: FloatingActionButton.extended(
         icon: const Icon(Icons.add),
-        onPressed: () { Navigator.pushNamed(context, toolAddFormScreenRoute); },
+        onPressed: () {
+          Navigator.pushNamed(context, toolAddFormScreenRoute);
+        },
         label: Text('Afegir eina'),
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(10, 40, 10, 10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(('Disponibilitat:')),
-                SizedBox(height: 45),
-              ],
-            ),
-            SingleChildScrollView(
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: tools.length,
-                itemBuilder: (context, index) {
-                  final tool = tools[index];
-                  return Column(
-                    children: [
-                      ListTile(
-                        title: Text(tool.title),
-                        subtitle: Text(tool.description),
-                        leading: Switch(
-                          value: tool.isAvailable,
-                          activeTrackColor: Colors.white10,
-                          activeColor: Colors.blue,
-                          onChanged: (value) {
-                            ref.read(allToolsProvider.notifier).updateTool(tool.copyWith(isAvailable: value));
-                          },
-                        ),
-                        trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              PopupMenuButton(
-                                icon: Icon(Icons.edit),
-                                itemBuilder: (context) => [
-                                  PopupMenuItem(
-                                    value: tool,
-                                    child: Text("Editar eina"),
+      body: tools.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : RefreshIndicator(
+            onRefresh: _refresh,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(('Disponibilitat:')),
+                        SizedBox(height: 45),
+                      ],
+                    ),
+                    ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: tools.length,
+                    itemBuilder: (context, index) {
+                      final tool = tools[index];
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Text(tool.title),
+                            subtitle: Text(tool.description),
+                            leading: Switch(
+                              value: tool.isAvailable,
+                              activeTrackColor: Colors.white10,
+                              activeColor: Colors.blue,
+                              onChanged: (value) {
+                                ref.read(allToolsProvider.notifier).updateTool(
+                                    tool.copyWith(isAvailable: value));
+                              },
+                            ),
+                            trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  PopupMenuButton(
+                                    icon: Icon(Icons.edit),
+                                    itemBuilder: (context) =>
+                                    [
+                                      PopupMenuItem(
+                                        value: tool,
+                                        child: Text("Editar eina"),
+                                      ),
+                                    ],
+                                    onSelected: (value) {
+                                      if (value == tool) {
+                                        Navigator.pushNamed(
+                                            context, toolEditCardScreenRoute,
+                                            arguments: EditToolArguments(
+                                                tool.id!));
+                                      }
+                                    },
                                   ),
-                                ],
-                                onSelected: (value){
-                                  if(value == tool) {
-                                    Navigator.pushNamed(context, toolEditCardScreenRoute,
-                                        arguments: EditToolArguments(tool.id!));
-                                  }
-                                  },
-                              ),
-                              PopupMenuButton(
-                                icon: Icon(Icons.delete),
-                                onSelected:  _deleteTool,
-                                  itemBuilder: (context) => [
-                                    PopupMenuItem(
-                                      value: tool,
-                                      child: Text("Esborrar de la llista?"),
-                                    ),
-                                  ],
-                              ),
-                            ]),
-                        onTap: () {
-                          Navigator.pushNamed(context, toolDetailScreenRoute, arguments: ToolDetailArguments(tool.id!));
-                        },
-                      ),
-                      Divider(),
-                    ],
-                  );
-                },
+                                  PopupMenuButton(
+                                    icon: Icon(Icons.delete),
+                                    onSelected: _deleteTool,
+                                    itemBuilder: (context) =>
+                                    [
+                                      PopupMenuItem(
+                                        value: tool,
+                                        child: Text("Esborrar de la llista?"),
+                                      ),
+                                    ],
+                                  ),
+                                ]),
+                            onTap: () {
+                              Navigator.pushNamed(context, toolDetailScreenRoute,
+                                  arguments: ToolDetailArguments(tool.id!));
+                            },
+                          ),
+                          Divider(),
+                        ],
+                      );
+                    },
+                  ),
+                ]),
               ),
-            ),
-          ],
-        ),
-      ),
+           // ],
+          ),
+        //),
+     // ),
     );
   }
 }

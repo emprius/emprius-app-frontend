60fa61a6d4c6b2ab7207cfe4c4cf850838d54cc4
MayuRuru <aruizrull@protonmail.com>
2022-09-29 13:28:09 +0200

    Change UI of rating cards

--- lib/src/features/rating/presentation/widgets/rating_card.dart
+++ lib/src/features/rating/presentation/widgets/rating_card.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:empriusapp/src/core/common_widgets/custom_text_button.dart';
 import 'package:empriusapp/src/core/common_widgets/rating_stars.dart';
+import 'package:empriusapp/src/core/helper/utils/date_utils.dart';
 import 'package:empriusapp/src/core/routes.dart';
 import 'package:empriusapp/src/features/bookings/application/providers/bookings_providers.dart';
 import 'package:empriusapp/src/features/rating/application/rating_provider.dart';
@@ -18,30 +20,51 @@ class RatingCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    //var newRating = 0;
-
     return Card(
       clipBehavior: Clip.antiAlias,
       child: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ListTile(
+            isThreeLine: true,
             leading: CircleAvatar(
-                backgroundImage: FileImage(File(ratingModel.thumbnail!))),
-            title: Text("Valorar ${ratingModel.title!}"),
+              radius: 30,
+              backgroundImage: AssetImage(ratingModel.thumbnail!),
+            ),
+            title: Row(
+              children: [
+                Text("Valoracio: "),
+                Text(
+                  "${ratingModel.title!}",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+                ),
+              ],
+            ),
             subtitle: Consumer(builder: (context, ref, _) {
               var booking =
                   ref.read(bookingByIdProvider(ratingModel.bookingId!));
-              return Text(
-                "Dates d'intercanvi: ${booking.startDate} / ${booking.endDate}",
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
+              return Container(
+                padding: EdgeInsets.all(6.0),
+                margin: EdgeInsets.all(4.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 0.5),
+                    borderRadius: const BorderRadius.all(Radius.circular(8.0))),
+                child: Row(mainAxisSize: MainAxisSize.min, children: [
+                  const Icon(Icons.calendar_month_outlined),
+                  const SizedBox(width: 8),
+                  Flexible(
+                    child: Text(
+                        "${getFormattedDate(booking.reservedDates!.start)} : ${getFormattedDate(booking.reservedDates!.end)} "),
+                  ),
+                ]),
               );
             }),
             trailing: Column(
               mainAxisSize: MainAxisSize.min,
-              children:  [
-                if(ratingModel.isPending == false)
-                 Icon(Icons.check_box_outlined, color: Colors.purple),
+              children: [
+                if (ratingModel.isPending == false)
+                  Icon(Icons.check_box_outlined, color: Colors.purple),
               ],
             ),
             onTap: () async {
@@ -49,78 +72,12 @@ class RatingCard extends StatelessWidget {
                   arguments: BookingDetailArguments(ratingModel.bookingId!));
             },
           ),
-          if(ratingModel.isPending!)
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Valorar:",
-                  style: TextStyle(color: Color(0xFF6200EE)),
-                ),
-
-                // TextButton(
-                //   child: const Text(
-                //     "Valorar:",
-                //     style: TextStyle(color: Color(0xFF6200EE)),
-                //   ),
-                //   onPressed: () async {
-                //     return await showDialog(
-                //         context: context,
-                //         builder: (BuildContext context) {
-                //           return Consumer(
-                //             builder: (context, ref, _) {
-                //               return AlertDialog(
-                //                 content: Column(
-                //                     crossAxisAlignment:
-                //                         CrossAxisAlignment.center,
-                //                     mainAxisSize: MainAxisSize.min,
-                //                     children: [
-                //                       Text("Valorar ${ratingModel.title}"),
-                //                       //buildRating(),
-                //                       RatingBar.builder(
-                //                         initialRating: newRating!,
-                //                         itemSize: 20,
-                //                         minRating: 1,
-                //                         itemCount: 5,
-                //                         allowHalfRating: true,
-                //                         itemBuilder: (context, _) => const Icon(
-                //                             Icons.star,
-                //                             color: Colors.amberAccent,
-                //                         size: 10,
-                //                         ),
-                //                         updateOnDrag: true,
-                //                         onRatingUpdate: (value) =>
-                //                             newRating = value,
-                //                       ),
-                //                     ]),
-                //                 actions: [
-                //                   TextButton(
-                //                     onPressed: () {
-                //                       Navigator.of(context).pop();
-                //                     },
-                //                     child: Text("Cancelar"),
-                //                   ),
-                //                   TextButton(
-                //                     child: Text("Guarda"),
-                //                     onPressed: () {
-                //                       ref
-                //                           .read(allRatingsProvider.notifier)
-                //                           .doRating(ratingModel.copyWith(
-                //                               rating: newRating));
-                //                       ref
-                //                           .read(allRatingsProvider.notifier)
-                //                           .doRating(ratingModel.copyWith(
-                //                               isPending: false));
-                //                       Navigator.pop(context);
-                //                     },
-                //                   ),
-                //                 ],
-                //               );
-                //             },
-                //           );
-                //         });
-                //   },
-                // ),
+          if (ratingModel.isPending!)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0.0, 10, 0),
+              child: ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                children: [
                   RatingBar.builder(
                     initialRating: newRating,
                     minRating: 1,
@@ -133,32 +90,28 @@ class RatingCard extends StatelessWidget {
                     onRatingUpdate: (value) => newRating = value,
                   ),
                   Consumer(builder: (context, ref, _) {
-                  var ratings = ref.watch(allRatingsProvider);
-
-                  //TODO CEHCK THIS
-                  return TextButton(
-                    child: Text("Guarda"),
-                    onPressed: () {
-                      ref
-                          .read(allRatingsProvider.notifier)
-                          .doRating(ratingModel, newRating);
+                    //var ratings = ref.watch(allRatingsProvider);
+                    return CustomTextButton(
                       // ref
                       //     .read(allRatingsProvider.notifier)
                       //     .doRating(ratingModel.copyWith(isPending: false));
-                      // Navigator.pop(context);
-                    },
-                  );
-                }),
-                //RatingStars(rating: ratingModel.rating!),
-              ],
-            )
+                      // Navigator.pop(context)},;
+                      text: 'GUARDA',
+                      onClicked: () {
+                        ref
+                            .read(allRatingsProvider.notifier)
+                            .doRating(ratingModel, newRating);
+                      },
+                    );
+                  }),
+                ]))
           else
             RatingBarIndicator(
               rating: ratingModel.rating!,
-              itemBuilder: (context, index) => Icon(
+              itemBuilder: (context, index) => const Icon(
                 Icons.star,
-                color:Colors.purple,
-    ),
+                color: Colors.purple,
+              ),
               itemSize: 25,
             ),
           const SizedBox(height: 10),
@@ -167,87 +120,3 @@ class RatingCard extends StatelessWidget {
     );
   }
 }
-
-// TextButton(
-//   onPressed: () async {
-//     await Navigator.pushNamed(context, bookingDetailScreenRoute,
-//         arguments: BookingDetailArguments(ratingModel.bookingId!));
-//   },
-//   child: Text(
-//     "Veure detall",
-//     style: TextStyle(color: Color(0xFF6200EE).withOpacity(0.6)),
-//   ),
-// ),
-
-//
-// Row(mainAxisSize: MainAxisSize.min, children: [
-// if (ratingModel.isPending == false) const Icon(Icons.check, size: 40),]),
-
-// Padding(
-//   padding: const EdgeInsets.fromLTRB(20, 2.0, 10, 5.0),
-//   child: Text(
-//     'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-//     style: TextStyle(color: Colors.black.withOpacity(0.6)),
-//   ),
-//),
-
-// TextButton(
-//   child: const Text(
-//     "Valorar:",
-//     style: TextStyle(color: Color(0xFF6200EE)),
-//   ),
-//   onPressed: () async {
-//     return await showDialog(
-//         context: context,
-//         builder: (BuildContext context) {
-//           return Consumer(
-//             builder: (context, ref, _) {
-//               return AlertDialog(
-//                 content: Column(
-//                     crossAxisAlignment:
-//                         CrossAxisAlignment.center,
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: [
-//                       Text("Valorar ${ratingModel.title}"),
-//                       //buildRating(),
-//                       RatingBar.builder(
-//                         initialRating: newRating!,
-//                         minRating: 1,
-//                         itemCount: 5,
-//                         allowHalfRating: true,
-//                         itemBuilder: (context, _) => const Icon(
-//                             Icons.star,
-//                             color: Colors.amberAccent),
-//                         updateOnDrag: true,
-//                         onRatingUpdate: (value) =>
-//                             newRating = value,
-//                       ),
-//                     ]),
-//                 actions: [
-//                   TextButton(
-//                     onPressed: () {
-//                       Navigator.of(context).pop();
-//                     },
-//                     child: Text("Cancelar"),
-//                   ),
-//                   TextButton(
-//                     child: Text("Guarda"),
-//                     onPressed: () {
-//                       ref
-//                           .read(allRatingsProvider.notifier)
-//                           .doRating(ratingModel.copyWith(
-//                               rating: newRating));
-//                       ref
-//                           .read(allRatingsProvider.notifier)
-//                           .doRating(ratingModel.copyWith(
-//                               isPending: false));
-//                       Navigator.pop(context);
-//                     },
-//                   ),
-//                 ],
-//               );
-//             },
-//           );
-//         });
-//   },
-// ),

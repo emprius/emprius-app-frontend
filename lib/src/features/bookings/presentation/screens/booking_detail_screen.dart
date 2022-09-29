8ade9a67d4cf4801e27908ff3a834270a42fac40
MayuRuru <aruizrull@protonmail.com>
2022-09-29 13:28:26 +0200

    Change UI of booking detail

--- lib/src/features/bookings/presentation/screens/booking_detail_screen.dart
+++ lib/src/features/bookings/presentation/screens/booking_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:empriusapp/src/core/common_widgets/custom_text_button.dart';
+import 'package:empriusapp/src/core/common_widgets/custom_textfield.dart';
 import 'package:empriusapp/src/core/common_widgets/rating_stars.dart';
 import 'package:empriusapp/src/core/common_widgets/status_label.dart';
 import 'package:empriusapp/src/core/helper/utils/date_utils.dart';
@@ -26,106 +28,131 @@ class _BookingDetailScreenState extends ConsumerState<BookingDetailScreen> {
     final booking = ref.watch(bookingByIdProvider(widget.args.bookingId));
     final tool = ref.watch(toolByIdProvider(booking.toolId!));
 
-    return booking == null
-        ? Container()
-        : Scaffold(
+    return Scaffold(
             appBar: UserAppbar("Reserva"),
-            body: Center(
+            body: Card(
               child: Padding(
-                padding: const EdgeInsets.fromLTRB(40.0, 0, 40.0, 10.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.fromLTRB(10.0, 0, 5.0, 10.0),
-                              child: CircleAvatar(
-                                backgroundImage:
-                                    FileImage(File(booking.userInfo!.avatar!)),
-                              ),
-                            ),
-                            SizedBox(height: 20.0),
-                            RatingStars(rating: booking.userInfo!.rating!),
-                          ]),
-                      SizedBox(height: 20.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        radius: 40,
+                        backgroundImage:
+                            AssetImage(booking.userInfo!.avatar!),
+                      ),
+                      title: Row(
                         children: [
-                          Column(
-                            children: [
-                              Text(
-                                  "L'usuaria ${booking.userInfo!.name} ha demanat:"),
-                              Text(
-                                tool.title,
-                                style: const TextStyle(
-                                  fontSize: 20.0,
-                                ),
-                              ),
-                            ],
+                          Text(
+                            "${booking.userInfo!.name}",
+                            style:
+                                TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
                           ),
-                          (booking.bookingStatus as BookingStatus).label
-                              as Widget,
+                          RatingStars(rating: booking.userInfo!.rating!),
                         ],
                       ),
-                      SizedBox(height: 20.0),
-                      Text(
-                          "Inici de la reserva: ${getFormattedDate(booking.reservedDates!.start)}"),
-                      Text(
-                          "Data de retorn: ${getFormattedDate(booking.reservedDates!.end)}"),
-                      SizedBox(height: 20.0),
-                      Text("Detalls de contacte:"),
-                      SizedBox(height: 20.0),
-                      Text(booking.contact),
-                      SizedBox(height: 20.0),
-                      if (booking.comments != null) Text(booking.comments!),
+                      subtitle: Text(booking.contact),
+                    ),
+                    Stack(children: [
+                      Center(
+                        child: Hero(
+                          tag: tool.images![0],
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.35,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  opacity: 0.8,
+                                  image: AssetImage(tool.images![0]),
+                                  fit: BoxFit.contain),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(6.0),
+                        margin: EdgeInsets.all(4.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(width: 0.5),
+                            borderRadius: const BorderRadius.all(Radius.circular(8.0))),
+                        child: Row(mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(Icons.calendar_month_outlined),
+                              const SizedBox(width: 8),
+                              Flexible(
+                                child: Text(
+                                    "${getFormattedDate(booking.reservedDates!.start)} : ${getFormattedDate(booking.reservedDates!.end)} "),
+                              ),
+                            ]),
+                      ),
                     ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(tool.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 30,
+                            )),
+                        const SizedBox(width: 8.0),
+                        (booking.bookingStatus).label as Widget,
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Text(
+                        booking.comments!,
+                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      ),
+                    ),
+                    booking.bookingStatus == BookingStatus.RETURNED
+                        ? Container()
+                        : ButtonBar(
+                            alignment: MainAxisAlignment.end,
+                            children: [
+                              if (booking.bookingStatus == BookingStatus.ASKED)
+                                CustomTextButton(
+                                  text: 'APROVA',
+                                  onClicked: () async {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content:
+                                              Text('Has acceptat la peticio!')),
+                                    );
+                                    await ref
+                                        .read(allBookingsProvider.notifier)
+                                        .editBooking(booking.copyWith(
+                                            bookingStatus:
+                                                BookingStatus.APPROVED));
+                                    if (!mounted) return;
+                                    Navigator.pushNamed(
+                                        context, userActivityScreenRoute);
+                                  },
+                                ),
+                              if (booking.bookingStatus == BookingStatus.APPROVED)
+                                CustomTextButton(
+                                  text: 'MARCA COM A RETORNADA',
+                                  onClicked: () async {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text('Eina retornada!')),
+                                    );
+                                    await ref
+                                        .read(allBookingsProvider.notifier)
+                                        .editBooking(booking.copyWith(
+                                            bookingStatus:
+                                                BookingStatus.RETURNED));
+                                    if (!mounted) return;
+                                    Navigator.pushNamed(
+                                        context, userActivityScreenRoute);
+                                  },
+                                ),
+                            ],
+                          ),
+                  ],
+                ),
               ),
-            ),
-            floatingActionButton: booking.bookingStatus ==
-                    BookingStatus.RETURNED
-                ? Container()
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      if (booking.bookingStatus == BookingStatus.ASKED)
-                        FloatingActionButton.extended(
-                          onPressed: () async {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text('Has acceptat la peticio!')),
-                            );
-                            await ref
-                                .read(allBookingsProvider.notifier)
-                                .editBooking(booking.copyWith(
-                                    bookingStatus: BookingStatus.APPROVED));
-                            if (!mounted) return;
-                            Navigator.pushNamed(
-                                context, userActivityScreenRoute);
-                          },
-                          label: Text("Aprova"),
-                        ),
-                      if (booking.bookingStatus == BookingStatus.APPROVED)
-                        FloatingActionButton.extended(
-                          onPressed: () async {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Eina retornada!')),
-                            );
-                            await ref
-                                .read(allBookingsProvider.notifier)
-                                .editBooking(booking.copyWith(
-                                    bookingStatus: BookingStatus.RETURNED));
-                            if (!mounted) return;
-                            Navigator.pushNamed(
-                                context, userActivityScreenRoute);
-                          },
-                          label: Text("Marca com a retornada"),
-                        ),
-                    ],
-                  ),
-          );
+            ));
   }
 }

66f6b1cc8aa437f449be574e426c084117d16941
MayuRuru <aruizrull@protonmail.com>
2023-01-06 18:08:36 +0100

    Add color scheme dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-
 import 'package:empriusapp/src/core/routes.dart';
 import 'package:empriusapp/src/core/services/storage/hive_storage_service.dart';
 import 'package:empriusapp/src/core/services/storage/storage_service.dart';
 import 'package:empriusapp/src/core/services/storage/storage_service_provider.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -32,7 +32,10 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const MaterialApp(
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+        theme: FlexColorScheme.light(scheme: FlexScheme.verdunHemlock).toTheme,
+        darkTheme: FlexColorScheme.dark(scheme: FlexScheme.verdunHemlock).toTheme,
         title: 'Emprius app',
         debugShowCheckedModeBanner: false,
         onGenerateRoute: Routes.generateRoute,

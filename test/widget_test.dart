09f3db8a7f44aafc373748f7b8b8f6f2e4c85615
MayuRuru <aruizrull@protonmail.com>
2023-01-12 19:36:54 +0100

    Try widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,26 @@
-/*
-import 'package:empriusapp/src/features/user/emprius_user/presentation/screens/user_tool_list_screen.dart';
-import 'package:empriusapp/src/features/user/emprius_user/presentation/widgets/user_appbar.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:empriusapp/main.dart';
-
-void main() {
-
-  testWidgets('verify title is displayed',
-          (WidgetTester tester) async {
-    await tester.pumpWidget(UserAppbar(title));
-
-    expect(find.text('Les meves eines'), findsOneWidget);
-  });
-}
-*/
+// import 'package:empriusapp/src/core/common_widgets/custom_text_button.dart';
+// import 'package:empriusapp/src/features/rating/domain/rating_model.dart';
+// import 'package:empriusapp/src/features/rating/presentation/widgets/rating_card.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_test/flutter_test.dart';
+//
+// void main() {
+//
+//   setUp(() => null)
+//
+//   testWidgets('given when then',
+//           (WidgetTester tester) async {
+//     var ratingModel = RatingModel();
+//     await tester.pumpWidget(
+//         ProviderScope(
+//             child: RatingCard(ratingModel),
+//             ));
+//     final button = find.text('VALORAR');
+//
+//     await tester.tap(button);
+//     await tester.pump();
+//     final checkedIcon = find.byIcon(Icons.check_box_outline_blank_outlined);
+//     expect(checkedIcon, findsOneWidget);
+//   });
+// }

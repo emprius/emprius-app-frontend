397185abff6ec6822384eee3bd5542227e734685
MayuRuru <aruizrull@protonmail.com>
2023-01-13 21:13:27 +0100

    WIP add tests

---
+++ test/core/common_widgets/status_label_test.dart
@@ -0,0 +1,20 @@
+/*
+import 'package:empriusapp/src/core/common_widgets/status_label.dart';
+import 'package:flutter/src/widgets/icon_data.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Status label has icon and color', (tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(const StatusLabel(icon: icon, color: color));
+
+    // Create the Finders.
+    IconData icon;
+    final iconFinder = find.byIcon(Icons.vi);
+
+    // Use the `findsOneWidget` matcher provided by flutter_test to
+    // verify that the Text widgets appear exactly once in the widget tree.
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
+  });
+}*/

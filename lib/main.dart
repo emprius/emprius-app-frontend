fb0aa961574bfe326c860f759f20a08ad85b9e97
MayuRuru <aruizrull@protonmail.com>
2023-01-23 13:31:46 +0100

    change theme data

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,39 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       themeMode: ThemeMode.system,
-        theme: FlexColorScheme.light(scheme: FlexScheme.verdunHemlock).toTheme,
-        darkTheme: FlexColorScheme.dark(scheme: FlexScheme.verdunHemlock).toTheme,
+      // This theme was made for FlexColorScheme version 6.1.1. Make sure
+// you use same or higher version, but still same major version. If
+// you use a lower version, some properties may not be supported. In
+// that case you can also remove them after copying the theme to your app.
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.verdunHemlock,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 9,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.verdunHemlock,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 15,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 20,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the Playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+// If you do not have a themeMode switch, uncomment this line
+// to let the device system mode control the theme mode:
+// themeMode: ThemeMode.system,
+
+      //theme: FlexColorScheme.light(scheme: FlexScheme.verdunHemlock).toTheme,
+        //darkTheme: FlexColorScheme.dark(scheme: FlexScheme.verdunHemlock).toTheme,
         title: 'Emprius app',
         debugShowCheckedModeBanner: false,
         onGenerateRoute: Routes.generateRoute,

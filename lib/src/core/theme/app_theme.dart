43f79a8c8c69a5503fdd604e36e1be7c77ae61ad
MayuRuru <aruizrull@protonmail.com>
2023-01-24 18:40:38 +0100

    Add fonts and build theme for colors and text

---
+++ lib/src/core/theme/app_theme.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+//final ThemeData empriusTheme = _empriusTheme();
+//final TextTheme empriusTextTheme = _empriusTextTheme();
+
+/*ThemeData _empriusTheme(){
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: const Color(0xFF616200),
+      primaryContainer: const Color(0xFFE7E885),
+      onPrimary: const Color(0xFFFFFFFF),
+      onPrimaryContainer: const Color(0xFF13130C),
+      secondary: const Color(0xFF606042),
+      onSecondary: const Color(0xFFFFFFFF),
+      onSecondaryContainer: const Color(0xFF131310),
+      secondaryContainer: const Color(0xFFE6E4C0),
+      tertiary: const Color(0xFF3D6657),
+      tertiaryContainer: const Color(0xFFBFECD9),
+      onTertiary: const Color(0xFFFFFFFF),
+      onTertiaryContainer: const Color(0xFF101412),
+      error: const Color(0xFFB00020),
+      errorContainer: const Color(0xFFFCD8DF),
+      onError: const Color(0xFFFFFFFF),
+      onErrorContainer: const Color(0xFF141213),
+      background: const Color(0xFFF9F9F6),
+      onBackground: const Color(0xFF090909),
+      inversePrimary: const Color(0xFFD5D58C),
+    ),
+    textTheme: _empriusTextTheme(base.textTheme),
+  );
+}*/
+
+/*
+
+TextTheme empriusTextTheme() {
+  final TextTheme textTheme = TextTheme();
+  return textTheme.copyWith(
+// This'll be our appbars title
+  headline1: textTheme.headline1!.copyWith(
+      fontFamily: "Montserrat",
+      fontSize: 30,
+      fontWeight: FontWeight.w500,
+      color: Colors.white),
+// for widgets heading/title
+  headline2: textTheme.headline2!.copyWith(
+    fontFamily: "Montserrat",
+    fontSize: 26,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  ),
+// for sub-widgets heading/title
+  headline3: textTheme.headline3!.copyWith(
+    fontFamily: "Montserrat",
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  ),
+// for widgets contents/paragraph
+  bodyText1: textTheme.bodyText1!.copyWith(
+      fontFamily: "Montserrat",
+      fontSize: 20,
+      fontWeight: FontWeight.w300,
+      color: Colors.black),
+// for sub-widgets contents/paragraph
+  bodyText2: textTheme.bodyText2!.copyWith(
+      fontFamily: "Montserrat",
+      fontSize: 18,
+      fontWeight: FontWeight.w300,
+      color: Colors.black),
+);
+}*/

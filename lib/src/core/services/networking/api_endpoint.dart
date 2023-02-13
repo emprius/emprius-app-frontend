1ddb33eba98ef593b8782e3b8e83e7ee35476a91
MayuRuru <aruizrull@protonmail.com>
2023-02-13 17:42:46 +0100

    Add api_endpoint.dart

---
+++ lib/src/core/services/networking/api_endpoint.dart
@@ -0,0 +1,123 @@
+import 'package:empriusapp/src/core/config/config.dart';
+import 'package:flutter/material.dart';
+
+/// A utility class for getting paths for API endpoints
+/// without involving strings.
+/// This class has no constructor and all methods are `static`.
+@immutable
+class ApiEndpoint {
+  const ApiEndpoint._();
+
+  /// The base url of our REST API, to which all the requests will be sent.
+  /// It is supplied at the time of building the apk or running the app:
+  /// ```
+  /// flutter build apk --debug --dart-define=BASE_URL=www.some_url.com
+  /// ```
+  /// OR
+  /// ```
+  /// flutter run --dart-define=BASE_URL=www.some_url.com
+  /// ```
+  static const baseUrl = Config.baseUrl;
+
+  /// Returns the path for an authentication [endpoint].
+  static String auth(AuthEndpoint endpoint) {
+    const path = '/auth';
+    switch (endpoint) {
+      case AuthEndpoint.REGISTER: return '$path/register';
+      case AuthEndpoint.LOGIN: return '$path/login';
+      case AuthEndpoint.REFRESH_TOKEN: return '$path/refresh-token';
+      case AuthEndpoint.CHANGE_PASSWORD: return '$path/change-password';
+      case AuthEndpoint.FORGOT_PW_SEND_OTP: return '$path/forgot/send-otp';
+      case AuthEndpoint.FORGOT_PW_VERIFY_OTP: return '$path/forgot/verify-otp';
+      case AuthEndpoint.FORGOT_PW_RESET_PASSWORD: return '$path/forgot/reset-password';
+    }
+  }
+
+  /// Enums for different endpoints collections:
+  ///
+  /// Returns the path for a user bookings [endpoint].
+  ///
+  /// Specify student connection [id] to get the path for a specific student connection.
+  static String userConnections(StudentConnectionEndpoint endpoint, {int? id}) {
+    const path = '/user-connections';
+    switch(endpoint){
+      case StudentConnectionEndpoint.BASE: return path;
+      case StudentConnectionEndpoint.REQUESTS: return '$path/bookings';
+      case StudentConnectionEndpoint.BY_ID: {
+        assert(id != null, 'userBookingsId is required for BY_ID endpoint');
+        return '$path/$id';
+      }
+    }
+  }
+
+  /// Returns the path for interests [endpoint].
+  static String interests(InterestEndpoint endpoint) {
+    const path = '/interests';
+    switch (endpoint) {
+      case InterestEndpoint.BASE: return path;
+    }
+  }
+}
+
+/// A collection of endpoints used for authentication purposes.
+enum AuthEndpoint {
+  /// An endpoint for registration requests.
+  REGISTER,
+
+  /// An endpoint for login requests.
+  LOGIN,
+
+  /// An endpoint for token refresh requests.
+  REFRESH_TOKEN,
+
+  /// An endpoint for change password requests.
+  CHANGE_PASSWORD,
+
+  /// An endpoint for reset password requests.
+  FORGOT_PW_RESET_PASSWORD,
+
+  /// An endpoint for forget password otp requests.
+  FORGOT_PW_SEND_OTP,
+
+  /// An endpoint for verifying forgot password otp code.
+  FORGOT_PW_VERIFY_OTP,
+}
+
+/// A collection of endpoints used for students.
+enum StudentEndpoint {
+  /// An endpoint for students' collection requests.
+  BASE,
+
+  /// An endpoint for individual students requests.
+  BY_ERP,
+
+  /// An endpoint for a student's organized activities
+  ORGANIZED_ACTIVITIES,
+
+  /// An endpoint for a student's saved activities
+  SAVED_ACTIVITIES_BASE,
+
+  /// An endpoint for a student's specific saved activity
+  SAVED_ACTIVITIES_BY_ID,
+
+  /// An endpoint for a student's attended activities
+  ATTENDED_ACTIVITIES,
+}
+
+/// A collection of endpoints used for student connections.
+enum StudentConnectionEndpoint {
+  /// An endpoint for a student's connections' collection requests.
+  BASE,
+
+  /// An endpoint for a student's pending connection requests.
+  REQUESTS,
+
+  /// An endpoint for a student's individual connection's requests.
+  BY_ID,
+}
+
+/// A collection of endpoints used for interests.
+enum InterestEndpoint {
+  /// An endpoint for interests' collection requests.
+  BASE,
+}

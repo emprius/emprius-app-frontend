f0eefcaafe25b5e04e96f6f4f33862df48e03307
MayuRuru <aruizrull@protonmail.com>
2023-02-13 16:51:45 +0100

    Add response_model.dart

---
+++ lib/src/core/services/networking/response_model.dart
@@ -0,0 +1,43 @@
+// Helpers
+import 'package:empriusapp/src/core/helper/utils/constants.dart';
+import 'package:empriusapp/src/core/helper/utils/typedefs.dart';
+
+
+class ResponseModel<T> {
+  final _ResponseHeadersModel headers;
+  final T body;
+
+  const ResponseModel({
+    required this.headers,
+    required this.body,
+  });
+
+  factory ResponseModel.fromJson(JSON json) {
+    return ResponseModel(
+      headers: _ResponseHeadersModel.fromJson(
+        json['headers'] as JSON,
+      ),
+      body: json['body'] as T,
+    );
+  }
+}
+
+class _ResponseHeadersModel {
+  final bool error;
+  final String message;
+  final String? code;
+
+  const _ResponseHeadersModel({
+    required this.error,
+    required this.message,
+    this.code,
+  });
+
+  factory _ResponseHeadersModel.fromJson(JSON json) {
+    return _ResponseHeadersModel(
+      error: Constants.boolFromInt(json['error'] as int),
+      message: json['message'] as String,
+      code: json['code'] as String?,
+    );
+  }
+}

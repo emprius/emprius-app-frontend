839698e8022703bc58d25b54a33b9043e9800f6c
MayuRuru <aruizrull@protonmail.com>
2023-02-13 12:12:05 +0100

    Get shared preferences and create data repository

---
+++ lib/src/core/repositories/data_repository.dart
@@ -0,0 +1,72 @@
+import 'package:empriusapp/src/core/repositories/endpoints_data.dart';
+import 'package:empriusapp/src/core/services/api.dart';
+import 'package:empriusapp/src/core/services/api_service.dart';
+import 'package:empriusapp/src/core/services/data_cache_service.dart';
+import 'package:empriusapp/src/core/services/endpoint_data.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+
+class DataRepository {
+  DataRepository({required this.apiService, required this.dataCacheService});
+  final APIService apiService;
+  final DataCacheService dataCacheService;
+
+  late String _accessToken;
+
+  Future<EndpointData> getEndpointData(Endpoint endpoint) async =>
+      await _getDataRefreshingToken<EndpointData>(
+        onGetData: () => apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint),
+      );
+
+  EndpointsData getAllEndpointsCachedData() => dataCacheService.getData();
+
+  Future<EndpointsData> getAllEndpointsData() async {
+    final endpointsData = await _getDataRefreshingToken<EndpointsData>(
+      onGetData: _getAllEndpointsData,
+    );
+    // save to cache
+    await dataCacheService.setData(endpointsData);
+    return endpointsData;
+  }
+
+  Future<T> _getDataRefreshingToken<T>({required Future<T> Function() onGetData}) async {
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await onGetData();
+    } on Response catch (response) {
+      // if unauthorized, get access token again
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await onGetData();
+      }
+      rethrow;
+    }
+  }
+
+  Future<EndpointsData> _getAllEndpointsData() async {
+    final values = await Future.wait([
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+    return EndpointsData(
+      values: {
+        Endpoint.cases: values[0],
+        Endpoint.casesSuspected: values[1],
+        Endpoint.casesConfirmed: values[2],
+        Endpoint.deaths: values[3],
+        Endpoint.recovered: values[4],
+      },
+    );
+  }
+}
---
+++ lib/src/core/repositories/endpoints_data.dart
@@ -0,0 +1,17 @@
+import 'package:empriusapp/src/core/services/api.dart';
+import 'package:empriusapp/src/core/services/endpoint_data.dart';
+import 'package:flutter/foundation.dart';
+
+class EndpointsData {
+  EndpointsData({required this.values});
+  final Map<Endpoint, EndpointData> values;
+  EndpointData? get cases => values[Endpoint.cases];
+  EndpointData? get casesSuspected => values[Endpoint.casesSuspected];
+  EndpointData? get casesConfirmed => values[Endpoint.casesConfirmed];
+  EndpointData? get deaths => values[Endpoint.deaths];
+  EndpointData? get recovered => values[Endpoint.recovered];
+
+  @override
+  String toString() =>
+      'cases: $cases, suspected: $casesSuspected, confirmed: $casesConfirmed, deaths: $deaths, recovered: $recovered';
+}

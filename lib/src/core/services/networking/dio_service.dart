61fc0d4296cc592a5215e08ba05365bbd9e27d08
MayuRuru <aruizrull@protonmail.com>
2023-02-13 17:44:02 +0100

    Refact importing response model

--- lib/src/core/services/networking/dio_service.dart
+++ lib/src/core/services/networking/dio_service.dart
@@ -4,6 +4,7 @@ import 'package:empriusapp/src/core/helper/utils/constants.dart';
 
 // Helpers
 import 'package:empriusapp/src/core/helper/utils/typedefs.dart';
+import 'package:empriusapp/src/core/services/networking/response_model.dart';
 
 // Models
 // Exceptions
@@ -160,53 +161,5 @@ class DioService {
       extra: <String, dynamic>{...dioOptions.extra!, ..._cacheOptionsMap},
     );
     return options;
-  }
-}
-
-/// This class prevents us from dealing with a Map object of response
-/// which can lead to errors in case a key is mistyped.
-/// It also allows debugger to infer type of our parsed JSON model
-/// and offer better code-completion in IDE.
-/// It contains the following classes:
-
-/// One to represent the response and contains a generic data type
-/// that we pass at each request level:
-class ResponseModel<T> {
-  final _ResponseHeadersModel headers;
-  final T body;
-
-  const ResponseModel({
-    required this.headers,
-    required this.body,
-  });
-
-  factory ResponseModel.fromJson(JSON json) {
-    return ResponseModel(
-      headers: _ResponseHeadersModel.fromJson(
-        json['headers'] as JSON,
-      ),
-      body: json['body'] as T,
-    );
-  }
-}
-
-/// Other class to represent response headers:
-class _ResponseHeadersModel {
-  final bool error;
-  final String message;
-  final String? code;
-
-  const _ResponseHeadersModel({
-    required this.error,
-    required this.message,
-    this.code,
-  });
-
-  factory _ResponseHeadersModel.fromJson(JSON json) {
-    return _ResponseHeadersModel(
-      error: Constants.boolFromInt(json['error'] as int),
-      message: json['message'] as String,
-      code: json['code'] as String?,
-    );
   }
 }

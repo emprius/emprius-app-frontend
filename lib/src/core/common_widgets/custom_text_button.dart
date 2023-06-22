2269539ef965b70ba6bb09ddb03f1901ca0cdc50
selankon <selankon@selankon.xyz>
2023-06-22 17:09:54 +0200

    Refactor custom text button to accept a builder

--- lib/src/core/common_widgets/custom_text_button.dart
+++ lib/src/core/common_widgets/custom_text_button.dart
@@ -1,5 +1,5 @@
-import 'package:empriusapp/src/core/common_widgets/custom_circular_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CustomTextButton extends StatelessWidget {
   final String text;
@@ -7,26 +7,40 @@ class CustomTextButton extends StatelessWidget {
   final Color? color;
   final EdgeInsetsGeometry padding;
   final double borderRadius;
-  /// Shows the circular progres indicator if loading
+
+  /// If listener is defined wraps the button on a consumer
+  ///
+  /// Util to use the maybeWhen when the button interacts with a FutureState
+  /// provider
+  final Widget Function(BuildContext, WidgetRef, Widget?)? builder;
 
   const CustomTextButton({
     Key? key,
     required this.text,
     required this.onClicked,
+    this.builder,
     this.color,
     this.borderRadius = 8,
-    this.padding  = const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    this.padding = const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
   }) : super(key: key);
+
+  Widget _buildButton() => ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(padding),
+          shape: MaterialStateProperty.all(const StadiumBorder()),
+        ),
+        onPressed: onClicked,
+        child: Text(text),
+      );
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(
-        padding: MaterialStateProperty.all(padding),
-        shape: MaterialStateProperty.all(const StadiumBorder()),
-      ),
-      onPressed: onClicked,
-      child: Text(text),
-    );
+    if (builder != null) {
+      return Consumer(
+        builder: builder!,
+        child: _buildButton(),
+      );
+    }
+    return _buildButton();
   }
 }

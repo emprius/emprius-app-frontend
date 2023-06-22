19b890b625104bafc956fc6cc509e5a1ebdb064b
selankon <selankon@selankon.xyz>
2023-06-22 17:10:59 +0200

    Add login loader

--- lib/src/features/user/auth_user/presentation/login_screen.dart
+++ lib/src/features/user/auth_user/presentation/login_screen.dart
@@ -1,7 +1,8 @@
+import 'package:empriusapp/src/core/common_widgets/custom_circular_loader.dart';
 import 'package:empriusapp/src/core/common_widgets/custom_text_button.dart';
 import 'package:empriusapp/src/core/common_widgets/custom_textfield.dart';
-import 'package:empriusapp/src/core/helper/form_validator.dart';
 import 'package:empriusapp/src/core/config/routes.dart';
+import 'package:empriusapp/src/core/helper/form_validator.dart';
 import 'package:empriusapp/src/core/shared/states/future_state.codegen.dart';
 import 'package:empriusapp/src/features/user/auth_user/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +23,6 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     void onData(bool? isAuthenticated) {
       if (isAuthenticated != null && isAuthenticated) {
         // todo(kon): use better navigation: use pop until and let the widgets move you until profile screen
@@ -35,12 +35,12 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
 
     ref.listen<FutureState<bool?>>(
       authProvider,
-          (_, authState) => authState.whenOrNull(
-        data: onData,
-        failed: (reason) => ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Usuari o contrassenya incorrectes')),
-        )
-      ),
+      (_, authState) => authState.whenOrNull(
+          data: onData,
+          failed: (reason) => ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                    content: Text('Usuari o contrassenya incorrectes')),
+              )),
     );
 
     return Scaffold(
@@ -53,40 +53,47 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
               children: [
                 const Text('Benvinguda a Emprius app'),
                 Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: CustomTextField(
-                      controller: _emailCtrl,
-                      validator: FormValidator.emailValidator,
-                      labelText: "E-mail",
-                    ),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: CustomTextField(
+                    controller: _emailCtrl,
+                    validator: FormValidator.emailValidator,
+                    labelText: "E-mail",
+                  ),
                 ),
                 Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: CustomTextField(
-                      controller: _passwordCtrl,
-                      validator: FormValidator.passwordValidator,
-                      labelText: 'Mot de pas',
-                      obscureText: _isHidden,
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                            _isHidden ? Icons.visibility : Icons.visibility_off),
-                        onPressed: () {
-                          setState(() {
-                            _isHidden = !_isHidden;
-                          });
-                        },
-                      ),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: CustomTextField(
+                    controller: _passwordCtrl,
+                    validator: FormValidator.passwordValidator,
+                    labelText: 'Mot de pas',
+                    obscureText: _isHidden,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                          _isHidden ? Icons.visibility : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          _isHidden = !_isHidden;
+                        });
+                      },
                     ),
+                  ),
                 ),
                 SizedBox(
-                    child: CustomTextButton(
+                  child: CustomTextButton(
+                      builder:
+                          (BuildContext context, WidgetRef ref, Widget? child) {
+                        final authState = ref.watch(authProvider);
+                        return authState.maybeWhen(
+                          loading: () => CustomCircularLoader(),
+                          orElse: () => child!,
+                        );
+                      },
                       text: "Entra a l'app!",
                       onClicked: () {
                         if (_formKey.currentState!.validate()) {
                           ref.read(authProvider.notifier).login(
-                            password: _passwordCtrl.text,
-                            email: _emailCtrl.text
-                          );
+                              password: _passwordCtrl.text,
+                              email: _emailCtrl.text);
                         }
                         // else if (ref.read(currentUserProvider.notifier).authState is !AUTHENTICATED) {
                         //   ScaffoldMessenger.of(context).showSnackBar(
@@ -95,8 +102,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                         //   return;
                         // }
                         // Navigator.pushReplacementNamed(context, userProfileScreenRoute);
-                      }
-                    ),
+                      }),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 15),
@@ -104,8 +110,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                     alignment: Alignment.center,
                     child: GestureDetector(
                       onTap: () {
-                        Navigator.pushNamed(
-                            context, registerScreenRoute);
+                        Navigator.pushNamed(context, registerScreenRoute);
                       },
                       child: RichText(
                         text: const TextSpan(
@@ -115,10 +120,10 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                           ),
                           children: [
                             TextSpan(
-                                text: "Registra't",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    decoration: TextDecoration.underline),
+                              text: "Registra't",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline),
                             ),
                           ],
                         ),
